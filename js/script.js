84a6cb9e1f5fd01c9c69616a2348c3be36b9e41d
Darley <tiusdiux.com@gmail.com>
2022-09-16 04:16:37 -0500

    Added functionality to buttons with JavaScript

--- js/script.js
+++ js/script.js
@@ -1,88 +1,125 @@
-function getComputerChoice() {
-  let random =  Math.floor(Math.random() * 3) + 1;
-  let choice = (random === 1) ? 'Rock' :
-         (random === 2) ? 'Paper' :
-         (random === 3) ? 'Scissors':
-         'We have a problem here!';
-  return choice;
-}
-function getUserChoice() {
-  let choice = prompt(`Make your choice! #f255`);
-  return choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+const rpsNodeList = document.querySelectorAll('.rps-list > button');
+rpsNodeList.forEach((btn) => {
+			btn.addEventListener('click', () => {
+			  demo(btn.textContent);
+			} )
+	      } );
+
+const msg = document.querySelector('#result');
+
+let gameCount = 0;
+
+let computerCount = 0;
+let userCount = 0;
+
+// ---------------------------------------------------------
+// This function 'runs' all rhe code; returns nothing
+function demo(input) {
+  if (gameCount < 0 || gameCount > 5) {
+    msg.textContent = 'Hey!, we have a probleme here; please, say it to the stupid who code this ···';
+    return;
+  }
+  if (gameCount === 0) {
+    msg.textContent = 'Please, select One: ';
+  }
+
+  let [message, winner] = getWinner(input);
+  msg.textContent = message;
+
+  if (winner) {
+    (winner === 'Computer') ? computerCount++ :
+    (winner === 'User') ? userCount++ :
+    msg.textContent += '${an error with the winner}';
+  }
+  else {
+    msg.textContent += `\nNo Winners(-_-)`
+  }
+  gameCount++;
+
+  if (gameCount === 5) {
+    msg.textContent = `
+Computer: ${computerCount}
+You: ${userCount}
+    `;
+    if (computerCount === userCount) {
+      msg.textContent += `\nTied!`;
+    }
+    else if (computerCount > userCount) {
+      msg.textContent += `YOU LOSE`;
+    }
+    else {
+      msg.textContent += `YOU WIN`;
+    }
+
+    gameCount = 0;
+    computerCount = 0;
+    userCount = 0;
+  }
 }
 
-function playRound(computerChoice, userChoice) {
-  rps = ['Rock', 'Paper', 'Scissors'];
-  let usrResult;
+// ---------------------------------------------------------
+// This function just return a random number between 0 and 2
+function getComputerChoiceNumber() {
+  return Math.floor(Math.random() * 3);
+}
+
+// ---------------------------------------------------------
+// This function plays a round of a game; return the 'index' of the winner
+function play(computerChoiceNum, userChoiceNum) {
+  console.log('--------------------- function - play');
+  if (computerChoiceNum < 0 || userChoiceNum < 0 ||
+      computerChoiceNum > 2 || userChoiceNum > 2) {
+    return `Really?, some here looks bad:
+    computer:		${computerChoiceNum}
+    you:		${userChoiceNum}`;
+  }
+  if (computerChoiceNum === userChoiceNum) {
+    return 'Tied! -> Give it your best!';
+  }
+
+  if (computerChoiceNum === 2 && userChoiceNum === 0 ||
+      computerChoiceNum === 0 && userChoiceNum === 2) {
+    return (computerChoiceNum < userChoiceNum) ?
+	    computerChoiceNum :
+	    userChoiceNum;
+  } else {
+   return (computerChoiceNum > userChoiceNum) ?
+      	    computerChoiceNum :
+	    userChoiceNum;
+  }
+}
+
+// ---------------------------------------------------------
+// This function works with the data; returns the messages:
+function getWinner(userChoice) {
+  let rpsArray = ['Rock', 'Paper', 'Scissors'];
+  let wins;
   let winner;
   let loser;
 
-  let checkRound = (a,b) => ((a > b) ? [a, b, 'lose'] :
-                         (a == b) ? [,,'tied'] :
-                         [b,a,'win']
-                       );
+  let userChoiceNumber = rpsArray.indexOf(userChoice);
+  let computerChoiceNumber = getComputerChoiceNumber();
+  let computerChoice = rpsArray[computerChoiceNumber];
 
-  if (rps.includes(computerChoice) && rps.includes(userChoice)) {
-    let computerValue = rps.indexOf(computerChoice);
-    let userValue = rps.indexOf(userChoice);
-
-    if (computerValue === 0 && userValue === 2 ||
-        computerValue === 2 && userValue === 0) {
-      [loser, winner, usrResult] = checkRound(userValue, computerValue);
-    }
-    else {
-      [winner, loser, usrResult] = checkRound(computerValue, userValue);
-    }
-
-    usrResult = usrResult ?? 'no wins'
-    winner = (rps[winner]) ?? 'Nothing';
-    loser = (rps[loser]) ?? "...";
-    let str = `You ${usrResult}! ${winner} beats ${loser}`;
-
-    return [winner, str];
+  let playValue = play(computerChoiceNumber, userChoiceNumber);
+  
+  if (typeof playValue=== 'string') {
+    console.log(playValue +'\n');
+    return [playValue, ''];
   }
-  return [winner, "Please, enter a valid input, nobody wins..."];
+
+  if (playValue === computerChoiceNumber) {
+    winner = computerChoice;
+    loser = userChoice;
+    wins = 'Computer';
+  }
+  else if (playValue === userChoiceNumber) {
+    winner = userChoice;
+    loser = computerChoice;
+    wins = 'User';
+  }
+  let message = `${winner} beats ${loser}`
+  console.log(message +'\n'+ wins);
+  return [message, wins];
 }
 
-function game(play, computer, user) {
-  let computerScore = 0;
-  let userScore = 0;
-  let computerChoice;
-  let userChoice;
-  let playData;
-
-  for (let i = 1; i <= 5; i++) {
-    console.group(`${i}th round:`)
-    computerChoice = computer();
-    userChoice = user();
-    console.log(`Computer Choice          ${computerChoice}`);
-    console.log(`User Choice              ${userChoice}`);
-
-    playData = play(computerChoice, userChoice);
-    console.log(playData[1]);
-
-    (playData[0] === computerChoice) ? computerScore++ :
-    (playData[0] === userChoice) ? userScore++ :
-    console.log("Nobody wins...");
-    console.log(`Computer: ${computerScore}   -   User: ${userScore}`);
-    console.groupEnd('---')
-  }
-  (userScore > computerScore) ?
-    console.info(`
-      Final Result: YOU WIN!!
-      You:  ${userScore}
-      Computer: ${computerScore}
-    `) :
-  (userScore == computerScore) ?
-    console.info(`
-      Final Result: YOU TIED!!
-      Computer: ${computerScore}
-      You:  ${userScore}
-    `) :
-    console.info(`
-      Final Result: YOU LOSE!!
-      Computer: ${computerScore}
-      You:  ${userScore}
-    `)
-}
-game(playRound, getComputerChoice, getUserChoice);
